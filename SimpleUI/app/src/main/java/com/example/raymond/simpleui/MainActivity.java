893a80b15de4b03732c1489bfc7c5190fb82023f
rmf770609 <tienhungfong@gmail.com>
2016-03-07 20:16:27 +0800

    onClick

--- SimpleUI/app/src/main/java/com/example/raymond/simpleui/MainActivity.java
+++ SimpleUI/app/src/main/java/com/example/raymond/simpleui/MainActivity.java
@@ -2,12 +2,20 @@ package com.example.raymond.simpleui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main); // res/layout/activity_main.xml
     }
+
+    public void submit(View view)
+    {
+        Toast.makeText(this, "Hello World", Toast.LENGTH_LONG).show();
+    }
+
 }
